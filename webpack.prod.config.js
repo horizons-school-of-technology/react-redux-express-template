32d3a3dadac531ef29597cd43fe667699f1ee847
jchuprograms <jchuprograms@gmail.com>
2017-07-13 14:18:08 -0700

    move eslint to dev only

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,20 +1,42 @@
-const config = require('./webpack.config.js');
 const webpack = require('webpack');
+const path = require('path');
 
-config.plugins.push(
-    new webpack.DefinePlugin({
-        "process.env": {
-            "NODE_ENV": JSON.stringify("production")
-        }
-    })
-);
-
-config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: false
-        }
-    })
-);
-
-module.exports = config;
+module.exports = {
+    entry: [
+        './frontend/index'
+    ],
+    module: {
+        rules: [
+            { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.s?css$/, loader: 'style-loader!css-loader!sass-loader' },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.scss']
+    },
+    output: {
+        path: path.join(__dirname, '/public'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devtool: 'cheap-eval-source-map',
+    devServer: {
+        contentBase: './public',
+        hot: true
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+            "process.env": {
+                "NODE_ENV": JSON.stringify("production")
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+};
