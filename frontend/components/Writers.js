c177eba1eabcdcb914f5b97c67da658dfa674f6a
Lizzie Halper <lizzie.halper@gmail.com>
2017-08-01 11:23:05 -0700

    react router implemented

---
+++ frontend/components/Writers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Writers = ( ) => {
+    return (
+    <div>
+        <h1>Writers</h1>
+    </div>
+    );
+};
+
+export default Writers;
--- frontend/containers/Root.dev.js
+++ frontend/containers/Root.dev.js
@@ -1,16 +1,21 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {Provider} from 'react-redux';
-import AppContainer from './AppContainer.js';
+import { Provider } from 'react-redux';
+import Routes from '../components/Routes';
 import DevTools from './DevTools';
+// import Router from 'react-router';
+import { ConnectedRouter } from 'react-router-redux';
 
-export default function Root({ store }) {
+export default function Root({ store, history }) {
+    console.log(Provider, ConnectedRouter);
     return (
         <Provider store={store}>
-            <div>
-                <AppContainer />
-                <DevTools />
-            </div>
+            <ConnectedRouter history={history}>
+                <div>
+                    <Routes/>
+                    <DevTools />
+                </div>
+            </ConnectedRouter>
         </Provider>
     );
 }
