1bc671987b01942a312dfc014f47152e44bb2d57
bubbadaboss77 <rfeldman@bates.edu>
2017-08-01 11:41:58 -0700

    ok

---
+++ backend/models/models.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var mongoose = require('mongoose');
+// var passport
+// var findOrCreate = require('mongoose-find-or-create');
+var connect = process.env.MONGODB_URI;
+// var findOrCreate = require('mongoose-find-or-create');
+
+// If you're getting an error here, it's probably because
+// your connect string is not defined or incorrect.
+mongoose.connect(connect);
+
+var Schema = mongoose.Schema;
+
+
+
+var userSchema = new Schema({
+  username: String,
+  password: String,
+  booksOwned: Array,  //array of document ID's
+});
+
+var User = mongoose.model('User', userSchema);
+
+
+module.exports = {
+  User: User
+};
