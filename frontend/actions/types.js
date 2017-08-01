b4d613b7069dd43ee62109a97781e76fda4bf941
Lizzie Halper <lizzie.halper@gmail.com>
2017-08-01 14:35:34 -0700

    commit

--- frontend/actions/types.js
+++ frontend/actions/types.js
@@ -1,1 +1,27 @@
 /* Action types */
+const login = (user, token, refresh) => {
+  return {
+    type: 'LOGIN',
+    user: user,
+    token: token,
+    refresh: refresh
+  };
+};
+
+const logout = () => {
+  return {
+    type: 'LOGOUT'
+  };
+};
+
+const addBook = () => {
+  return {
+    type: 'ADD_BOOK'
+  };
+};
+
+export default {
+  login: login,
+  logout: logout,
+  addBook: addBook,
+};
--- frontend/components/Routes.js
+++ frontend/components/Routes.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import { Switch, Router, Redirect, Route } from 'react-router';
 import AppContainer from '../containers/AppContainer';
 import App from './App';
-import Students from './Students';
+import Login from '../containers/Login';
 import Writers from './Writers';
 
 class Routes extends Component {
     render() {
-        console.log("STUDENTS COMPONENT", Students)
         return (
             <App>
                 <Switch>
                     <Route exact path='/' component={AppContainer}/>
-                    <Route path='/students' component={Students}/>
+                    <Route path='/students' component={Login}/>
                     <Route path='/writers' component={Writers}/>
                 </Switch>
             </App>
