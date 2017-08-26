42f05dfa040f73e0aea6a6fb56228fa86fc35a57
Antonio Chan <antonio.chan@emory.edu>
2017-08-25 23:52:52 -0400

    changed title

--- test/Title.js
+++ test/Title.js
@@ -7,10 +7,10 @@ import Title from '../frontend/components/Title';
 describe('Component: App', () => {
   it('should render the App component', () => {
     const wrapper = shallow(
-      <Title name="Horizons" />
+      <Title name="Emory Course Review" />
     );
 
-    expect(wrapper.find('h1').text()).toEqual('Horizons');
+    expect(wrapper.find('h1').text()).toEqual('Emory Course Review');
   });
 
   it('should run a solid smoke test', () => {
