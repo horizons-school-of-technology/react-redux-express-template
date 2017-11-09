e6ac0b868530212a438725f431b671ad17f29817
crawforde <crawforde@kenyon.edu>
2017-11-09 11:50:22 -0800

    Fixed history problem. Run npm install again.

--- frontend/store/configureStore.dev.js
+++ frontend/store/configureStore.dev.js
@@ -1,6 +1,9 @@
+import createHistory from 'history/createBrowserHistory';
 import { createStore, compose } from 'redux';
 import rootReducer from '../reducers';
 import DevTools from '../containers/DevTools';
+
+export const history = createHistory();
 
 export function configureStore(initialState) {
     return createStore(
