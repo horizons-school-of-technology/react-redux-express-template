02d3f88f4f24cd83175296e6f4321e278967caf9
Brock <bws219@lehigh.edu>
2017-11-09 14:00:35 -0800

    setup sequelize skeleton

---
+++ models.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(process.env.DATABASE_NAME, 'postgres', process.env.DATABASE_PASSWORD, {
+    dialect: 'postgres'
+});
+
+sequelize
+.authenticate()
+.then(() => {
+    console.log('Connection has been established successfully.');
+})
+.catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
+
+// MODELS GO HERE
+
+module.exports = {
+    sequelize,
+    // EXPORT models HERE
+};
