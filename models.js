6be43ce220605827f374b2a737442fe1d7cf81be
Emma Paschke <epaschke>
2017-11-09 14:20:57 -0800

    passport

---
+++ models.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(process.env.DATABASE_NAME, 'postgres', process.env.DATABASE_PASSWORD, {
+    dialect: 'postgres'
+});
+
+sequelize
+.authenticate()
+.then(() => {
+    console.log('Connection has been established successfully.');
+})
+.catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
+
+const User = sequelize.define('user', {
+    id: { type: Sequelize.INTEGRER, primaryKey: true, autoIncrement: true },
+    username: { type: Sequelize.STRING, allowNull: false, unique: true },
+    password: { type: Sequelize.STRING, allowNull: false },
+    img_url: Sequelize.STRING
+});
+
+const Post = sequelize.define('post', {
+    id: { type: Sequelize.INTEGRER, primaryKey: true, autoIncrement: true },
+    title: Sequelize.STRING,
+    content: { type: Sequelize.STRING(1234), allowNull: false }
+});
+
+const Vote = sequelize.define('vote', {
+    id: { type: Sequelize.INTEGRER, primaryKey: true, autoIncrement: true },
+    up: {type: Sequelize.BOOLEAN, allowNull: false }
+});
+
+Post.belongsTo(User, { foreignKey: { allowNull: false} });
+Post.belongsTo(Post);
+Vote.belongsTo(Post, { foreignKey: { allowNull: false } });
+Vote.belongsTo(User, { foreignKey: { allowNull: false } });
+// Post.belongsTo(SubReddit, { foreignKey: { allowNull: false } });
+
+module.exports = {
+    User,
+    Post,
+    Vote,
+    sequelize
+};
