cb1e028edb10930d8d5b6a25cb933f25a4258069
Brock <bws219@lehigh.edu>
2017-11-09 18:37:06 -0800

    day 1 complete

---
+++ frontend/components/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Posts extends React.Component {
+  componentWillMount() {
+    <h1>Filler</h1>;
+  }
+  render() {
+    return (
+          <div />
+    );
+  }
+}
+
+export default Posts;
--- frontend/containers/Root.dev.js
+++ frontend/containers/Root.dev.js
@@ -4,20 +4,22 @@ import {Provider} from 'react-redux';
 import AppContainer from './AppContainer.js';
 // import DevTools from './DevTools';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-
+import Login from './LoginContainer';
 
 export default function Root({ store }) {
-    return (
+  return (
         <Provider store={store}>
           <HashRouter>
             <Switch>
               <Route exact path="/" component={AppContainer} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Login} />
             </Switch>
           </HashRouter>
         </Provider>
-    );
+  );
 }
 
 Root.propTypes = {
-    store: PropTypes.object.isRequired
+  store: PropTypes.object.isRequired
 };
